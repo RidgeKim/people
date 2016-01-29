efaa25833bb6d0cb3a0d40c265d86e41e4c2cf8e
Ian Juma <ian.juma@yahoo.com>
2016-01-30 00:02:26 +0300

    added run_query for elasticSearch

--- app/run_query_es.py
+++ app/run_query_es.py
@@ -1,5 +1,28 @@
 import requests
 
+import json
 
-def full_text_search(search_by='name'):
-    pass
+people = open("people.json").read()
+
+data = json.loads(people)
+
+base_url = "GET http://localhost:9200/users/people/_search?%s=%s"
+
+
+def full_text_search(some_value, search_by='name'):
+	url = base_url %(search_by, some_value)
+	response = requests.get(url)
+
+	return response.text
+
+
+if __name__ == "__main__":
+
+    for person in data:
+        print person.get('id'), person.get('gender')
+
+        try:
+        	full_text_search(person.get('name'))
+
+        except Exception as f:
+            print f
